e84a1b6b11d28bfde3179a91f7be7210add798ef
Kenluckyz <aoken0991242405@gmail.com>
2026-03-26 10:23:56 +0700

    Script

--- Assets/Script/Gravitation.cs
+++ Assets/Script/Gravitation.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Gravitation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static List<Gravitation> otherObj;
+    private Rigidbody _rb;
+    const float G = 0.00667f;
+    void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        if (otherObj == null)
+        {
+            otherObj = new List<Gravitation>();
+        }
+        otherObj.Add(this);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        foreach (Gravitation obj in otherObj)
+        {
+            if (obj != this)
+            {
+                Attract(obj);
+            }
+        }
+        void Attract(Gravitation other)
+        {
+            Rigidbody otherRb = other._rb;
+            Vector3 direction = _rb.position - otherRb.position;
+
+            float distance = direction.magnitude;
+            if (distance == 0f) return;
+
+            // F = G(m1 * m2) / r^2
+            float forceMagnitude = G * (_rb.mass * otherRb.mass) / Mathf.Pow(distance, 2);
+            Vector3 gravitationForce = forceMagnitude * direction.normalized;
+            otherRb.AddForce(gravitationForce);
+        }
     }
 }
+
